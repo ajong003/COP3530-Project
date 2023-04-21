5e6bb5e1ef6904c3347b2accea4fe392c97ff46f
ajong003 <ajong003@fiu.edu>
2023-04-21 18:17:47 -0400

    sdgs

--- Project Card/src/Main.java
+++ Project Card/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
             //create game instance
             JackOfAllTrades game = new JackOfAllTrades();
+
             ////
     }
 }
